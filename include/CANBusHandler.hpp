a9d9a8b31a36d9b37b100f3c0fb65fa2b712e662
Rui Pires <ruipedro.pires999@gmail.com>
2025-01-09 11:50:22 +0000

    Update CMake configuration and refactor CANBusHandler for zenoh integration

--- include/CANBusHandler.hpp
+++ include/CANBusHandler.hpp
@@ -1,44 +1,35 @@
 #ifndef CANBUSHANDLER_HPP
 #define CANBUSHANDLER_HPP
 
-#include <QtCore/QObject>
-#include <QtCore/QDebug>
-#include <QtCore/QTimer>
-#include <sys/socket.h>
-#include <linux/can.h>
-#include <linux/can/raw.h>
-#include <net/if.h>
-#include <sys/ioctl.h>
-#include <unistd.h>
-#include <arpa/inet.h>
+#include <QObject>
+#include <iostream>
+#include "zenoh.hxx"
+
+using namespace zenoh;
 
 class CANBusHandler : public QObject
 {
     Q_OBJECT
     Q_PROPERTY(int speed READ getSpeed NOTIFY speedChanged)
-    Q_PROPERTY(
-        int battery READ getBattery NOTIFY batteryChanged)
+    Q_PROPERTY(int battery READ getBattery NOTIFY batteryChanged)
 
   public:
-    explicit CANBusHandler(QObject* parent = nullptr);
+    explicit CANBusHandler(Session& session, QObject* parent = nullptr);
     ~CANBusHandler();
 
     int getSpeed() const;
-    void setSpeed(int speed);
     int getBattery() const;
+    void setSpeed(int speed);
     void setBattery(int battery);
 
   signals:
     void speedChanged(int speed);
     void batteryChanged(int battery);
 
-  private slots:
-    void readFrames();
-
   private:
-    int canSocket;
     int m_speed;
     int m_battery;
+    Session& m_session;
 };
 
 #endif // CANBUSHANDLER_HPP

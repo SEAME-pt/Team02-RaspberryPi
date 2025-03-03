dcc73ead3936adde7529d94d288188f67d45ef10
Rui Pires <ruipedro.pires999@gmail.com>
2025-03-03 15:19:59 +0000

    Add Zenoh configuration files and enhance deployment script for Raspberry Pi

--- deploy/scripts/deployToRasp.sh
+++ deploy/scripts/deployToRasp.sh
@@ -4,11 +4,24 @@ projectDir=RaspberryPi
 piUserName=team02
 piIpAddressLocal=10.21.221.64
 piIpAddressRemote=10.21.221.19
-piPath=/home/team02
+piPathBin=/opt/vehicle/bin
+piPathEtc=/opt/vehicle/etc/zenoh
 piPass=seameteam2
-
 architecture=$(uname -m)
-echo "Detected architecture: $architecture"
+
+check_ssh_connection() {
+    local host=$1
+    local user=$2
+    echo "Checking connection to $host..."
+    # Use nc (netcat) to test connection with 5 second timeout
+    if nc -G 5 -z "$host" 22 >/dev/null 2>&1; then
+        # Test SSH login
+        if sshpass -p "$piPass" ssh -q -o ConnectTimeout=5 "$user@$host" exit; then
+            return 0
+        fi
+    fi
+    return 1
+}
 
 # Build docker image with appropriate platform flag
 echo "Building docker image to build app..."
@@ -33,7 +46,31 @@ echo "Copy the binary from tmp container"
 docker cp tmpapp:/home/$projectDir/InstrumentClusterApp ./InstrumentClusterApp
 docker cp tmpapp:/home/$projectDir/HandClusterApp ./HandClusterApp
 docker cp tmpapp:/home/$projectDir/MiddleWareApp ./MiddleWareApp
-echo "Send binary to local rasp over scp"
-sshpass -p "$piPass" scp InstrumentClusterApp MiddleWareApp "$piUserName"@"$piIpAddressLocal":"$piPath"
-echo "Send binary to remote rasp over scp"
-sshpass -p "$piPass" scp HandClusterApp "$piUserName"@"$piIpAddressRemote":"$piPath"
+
+if check_ssh_connection "$piIpAddressLocal" "$piUserName"; then
+    echo "Stopping services on local Raspberry Pi..."
+    sshpass -p "$piPass" ssh "$piUserName"@"$piIpAddressLocal" "sudo systemctl stop middleware.service && pkill InstrumentClus"
+    
+    echo "Send binary to local rasp over scp"
+    sshpass -p "$piPass" scp InstrumentClusterApp MiddleWareApp "$piUserName"@"$piIpAddressLocal":"$piPathBin"
+    sshpass -p "$piPass" scp ./$projectDir/ZenohConfig/InstrumentClusterConfig.json ./$projectDir/ZenohConfig/MiddleWareConfig.json "$piUserName"@"$piIpAddressLocal":"$piPathEtc"
+    
+    echo "Restarting services on local Raspberry Pi..."
+    sshpass -p "$piPass" ssh "$piUserName"@"$piIpAddressLocal" "sudo systemctl start middleware.service"
+
+    echo "InstrumentCluster will restart on next login"
+else
+    echo "ERROR: Cannot connect to local Raspberry Pi at $piIpAddressLocal"
+fi
+
+if check_ssh_connection "$piIpAddressRemote" "$piUserName"; then
+    echo "Stopping service on remote Raspberry Pi..."
+    sshpass -p "$piPass" ssh "$piUserName"@"$piIpAddressRemote" "pkill HandCluster"
+    
+    echo "Send binary to remote rasp over scp"
+    sshpass -p "$piPass" scp HandClusterApp "$piUserName"@"$piIpAddressRemote":"$piPathBin"
+    
+    echo "HandCluster will restart on next login"
+else
+    echo "ERROR: Cannot connect to remote Raspberry Pi at $piIpAddressRemote"
+fi

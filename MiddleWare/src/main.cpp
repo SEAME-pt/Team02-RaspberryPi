064d9593e6099e4184e148975a879d7820e0a112
ruipires <ruipedro.pirees999@gmail.com>
2025-07-16 09:19:36 +0100

    Format state_of_charge_publish method for improved readability

--- MiddleWare/src/main.cpp
+++ MiddleWare/src/main.cpp
@@ -206,7 +206,8 @@ public:
     }
     void state_of_charge_publish(const std::string& value_str) {
         const auto len = value_str.size() + 1;
-        auto alloc_result = provider_->alloc_gc_defrag_blocking(len, zenoh::AllocAlignment({0}));
+        auto alloc_result =
+            provider_->alloc_gc_defrag_blocking(len, zenoh::AllocAlignment({0}));
         zenoh::ZShmMut&& buf = std::get<zenoh::ZShmMut>(std::move(alloc_result));
         memcpy(buf.data(), value_str.c_str(), len);
         state_of_charge_publisher_->put(std::move(buf));
@@ -373,8 +374,6 @@ int main(int argc, char** argv)
     }
     
     SHMPublishers publishers(session);
-
-    
 
     while (1)
     {
